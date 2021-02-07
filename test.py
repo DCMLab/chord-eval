def83d597b8b4a20d9fc574943c77334725367d0
Xavier-Suermondt <71978345+Xavier-Suermondt@users.noreply.github.com>
2021-02-07 19:31:26 +0100

    test for caching using defaultdic
    test for the chord_SPS function using caching and defaultdic

--- test.py
+++ test.py
@@ -4,36 +4,34 @@
 from Chord_SPS_copy import chord_SPS
 from data_types import ChordType
 
-spect = {}
-
-d = chord_SPS(0, 0, ChordType.MAJOR, ChordType.MAJOR, transform='vqt', spectrogram=spect)
+d,spec = chord_SPS(0, 0, ChordType.MAJOR, ChordType.MAJOR, transform='vqt')
 print('The SPS of Cmaj and Cmaj is : {:.5}'.format(d))
-d = chord_SPS(0, 0, ChordType.MAJOR, ChordType.MAJOR, transform='vqt', spectrogram=spect)
+d,_ = chord_SPS(0, 0, ChordType.MAJOR, ChordType.MAJOR, transform='vqt', spectrogram=spec)
 print('The SPS of Cmaj and Cmaj is : {:.5}'.format(d))
 
-d = chord_SPS(0, 0, ChordType.MAJOR, ChordType.MINOR, transform='vqt', spectrogram=spect)
+d,_ = chord_SPS(0, 0, ChordType.MAJOR, ChordType.MINOR, transform='vqt', spectrogram=spec)
 print('The SPS of Cmaj and Cmin is : {:.5}'.format(d))
-d = chord_SPS(0, 0, ChordType.MAJOR, ChordType.MINOR, transform='vqt', spectrogram=spect)
+d,_ = chord_SPS(0, 0, ChordType.MAJOR, ChordType.MINOR, transform='vqt', spectrogram=spec)
 print('The SPS of Cmaj and Cmin is : {:.5}'.format(d))
 
-d = chord_SPS(0, 4, ChordType.MAJ_MAJ7, ChordType.MINOR, transform='vqt', spectrogram=spect)
+d,_ = chord_SPS(0, 4, ChordType.MAJ_MAJ7, ChordType.MINOR, transform='vqt', spectrogram=spec)
 print('The SPS of Cmaj7 and Emin is : {:.5}'.format(d))
-d = chord_SPS(0, 4, ChordType.MAJ_MAJ7, ChordType.MINOR, transform='vqt', spectrogram=spect)
+d,_ = chord_SPS(0, 4, ChordType.MAJ_MAJ7, ChordType.MINOR, transform='vqt', spectrogram=spec)
 print('The SPS of Cmaj7 and Emin is : {:.5}'.format(d))
 
-d = chord_SPS(0, 9, ChordType.MAJOR, ChordType.MIN_MIN7, transform='vqt', spectrogram=spect)
+d,_ = chord_SPS(0, 9, ChordType.MAJOR, ChordType.MIN_MIN7, transform='vqt', spectrogram=spec)
 print('The SPS of C4maj and A4min7 is : {:.5}'.format(d))
-d = chord_SPS(0, 9, ChordType.MAJOR, ChordType.MIN_MIN7, transform='vqt', spectrogram=spect)
+d,_ = chord_SPS(0, 9, ChordType.MAJOR, ChordType.MIN_MIN7, transform='vqt', spectrogram=spec)
 print('The SPS of C4maj and A4min7 is : {:.5}'.format(d))
 
-d = chord_SPS(0, -3, ChordType.MAJOR, ChordType.MIN_MIN7, transform='vqt', spectrogram=spect)
+d,_ = chord_SPS(0, -3, ChordType.MAJOR, ChordType.MIN_MIN7, transform='vqt', spectrogram=spec)
 print('The SPS of C4maj and A3min7 is : {:.5}'.format(d))
-d = chord_SPS(0, -3, ChordType.MAJOR, ChordType.MIN_MIN7, transform='vqt', spectrogram=spect)
+d,_ = chord_SPS(0, -3, ChordType.MAJOR, ChordType.MIN_MIN7, transform='vqt', spectrogram=spec)
 print('The SPS of C4maj and A3min7 is : {:.5}'.format(d))
 
-d = chord_SPS(0, 1, ChordType.MAJOR, ChordType.MAJOR, transform='vqt', spectrogram=spect)
+d,_ = chord_SPS(0, 1, ChordType.MAJOR, ChordType.MAJOR, transform='vqt', spectrogram=spec)
 print('The SPS of Cmaj and Dbmaj is : {:.5}'.format(d))
-d = chord_SPS(0, 1, ChordType.MAJOR, ChordType.MAJOR, transform='vqt', spectrogram=spect)
+d,_ = chord_SPS(0, 1, ChordType.MAJOR, ChordType.MAJOR, transform='vqt', spectrogram=spec)
 print('The SPS of Cmaj and Dbmaj is : {:.5}'.format(d))
 
 
