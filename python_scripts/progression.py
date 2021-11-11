a811975dc7d31b35fbbe1a091829846486de0efb
Andrew McLeod <apmcleod@gmail.com>
2021-11-11 17:26:29 +0100

    Cleaned up Data dir

--- python_scripts/progression.py
+++ python_scripts/progression.py
@@ -98,19 +98,19 @@ def test_columns(
     columns_df=df.columns
     if 'chord_root_midi' not in columns_df:
         raise KeyError("'chord_root_midi' must be a column of the data frame.")
-	
+
     if 'chord_type' not in columns_df:
         raise KeyError("'chord_type' must be a column of the data frame.")
-		
+
     if 'chord_inversion' not in columns_df:
         raise KeyError("'chord_inversion' must be a column of the data frame.")
-	
+
     if 'duration' not in columns_df:
         raise KeyError("'duration' must be a column of the data frame.")
-	
+
     if 'chord_suspension_midi' not in columns_df:
         raise KeyError("'chord_suspension_midi' must be a column of the data frame.")
-		
+
     df['duration'] = df['duration'].apply(lambda r : Fraction(r))
 
 
@@ -123,29 +123,29 @@ def get_progression(
 		tbt_kws:dict={}
 	) -> pd.DataFrame:
 	"""
-	Construct a pd.DataFrame where each row is a event : a chord change in one of 
+	Construct a pd.DataFrame where each row is a event : a chord change in one of
 	the annotation.
-	
+
 
 	Parameters
 	----------
 	df1 : pd.DataFrame
 		The first annotation of the piece.
-		
+
 	df2 : pd.DataFrame
 		The second annotation of the piece.
-		
+
 	triad_reduction : bool, optional
         If True, the function will reduce every input chords to their triad
         reduction.
         The default is False.
-		
+
 	sps_kws: Dict
 		Dictionary of keyword arguments for SPS_distance(). The defautl is {}.
-		
+
 	vl_kws: Dict
 		Dictionary of keyword arguments for voice_leading_distance(). The defautl is {}.
-		
+
 	tbt_kws: Dict
 		Dictionary of keyword arguments for tone_by_tone_distance(). The defautl is {}.
 
@@ -157,56 +157,63 @@ def get_progression(
 		and the distances between each annotation..
 
 	"""
-	
+
 	# Test if the two DataFrames contains the needed columns for the evaluation
 	test_columns(df1)
 	test_columns(df2)
-	
+
+	# Make df2 dcml
+	swap = False
+	if "label" in df1.columns:
+		swap = True
+		df1, df2 = df2, df1
+
 	# Creation of a new column containing the full description of each encontered
 	# chord in the annotations
 	df1_full_chord = df1.apply(lambda r : PITCH_TO_STRING[r.chord_root_midi] + '_' + str(r.chord_type).split(".")[1]+"_inv"+str(r.chord_inversion), axis=1)
 	df2_full_chord = df2.apply(lambda r : PITCH_TO_STRING[r.chord_root_midi] + '_' + str(r.chord_type).split(".")[1]+"_inv"+str(r.chord_inversion), axis=1)
-	
-	# Creation of a new column containing the duration interval of each encontered 
+
+	# Creation of a new column containing the duration interval of each encontered
 	# chord in the annotations in terme. The intervals are expressed in terms of
 	# whole notes and with respect of the begining of the piece.
 	time_df1 = df1.duration.cumsum().astype(float, copy=False)
 	df1_interval = [[i, f] for i, f in zip([0]+list(time_df1[:-1]), time_df1)]
-	
+
 	time_df2 = df2.duration.cumsum().astype(float, copy=False)
 	df2_interval = [[i, f] for i, f in zip([0]+list(time_df2[:-1]), time_df2)]
-	
+
 	# The columns of the output DataFrame
 	sps = []
 	vl  = []
 	tbt = []
 	binary = []
-		
+	dcml_label = []
+
 	time = []
 	durations = []
-		
+
 	chords_df2 = []
 	chords_df1 = []
-	
-	# For every encontered chord of one annotation, we find wich chord in the 
+
+	# For every encontered chord of one annotation, we find wich chord in the
 	# other annotation it matches with
 	idx_df1 = 0
 	for idx_df2, rdf2 in df2.iterrows():
-		
+
 		matched_idx = []
 		matched_duration = []
-		
+
 		chords_sps_dist = []
 		chords_vl_dist = []
 		chords_tbt_dist = []
 		chords_bin_dist = []
-	        
+
 		if (idx_df1 > 0 and overlap(df1_interval[idx_df1-1], df2_interval[idx_df2])):
-	            
+
 			matched_idx.append(idx_df1-1)
-	            
+
 			matched_duration.append(duration_overlap(df1_interval[idx_df1-1], df2_interval[idx_df2]))
-	            
+
 			chords_sps_dist.append(get_distance(distance = 'SPS',
 	                                               root1=df1.chord_root_midi[idx_df1-1],
 	                                               root2=rdf2.chord_root_midi,
@@ -219,7 +226,7 @@ def get_progression(
 												   triad_reduction=triad_reduction,
 												   **sps_kws
 												   ))
-	
+
 			chords_vl_dist.append(get_distance(distance = 'voice leading',
 	                                               root1=df1.chord_root_midi[idx_df1-1],
 	                                               root2=rdf2.chord_root_midi,
@@ -232,7 +239,7 @@ def get_progression(
 												   triad_reduction=triad_reduction,
 	                                               **vl_kws
 												   ))
-	
+
 			chords_tbt_dist.append(get_distance(distance = 'tone by tone',
 	                                               root1=df1.chord_root_midi[idx_df1-1],
 	                                               root2=rdf2.chord_root_midi,
@@ -245,15 +252,16 @@ def get_progression(
 												   triad_reduction=triad_reduction,
 												   **tbt_kws
 												   ))
-			
+
 			chords_bin_dist.append(0 if df1_full_chord[idx_df1-1]==df2_full_chord[idx_df2] else 1)
-	
+			dcml_label.append(rdf2.label)
+
 		while(idx_df1 < len(df1) and overlap(df1_interval[idx_df1], df2_interval[idx_df2])):
-	            
+
 			matched_idx.append(idx_df1)
-	            
+
 			matched_duration.append(duration_overlap(df1_interval[idx_df1], df2_interval[idx_df2]))
-	            
+
 			chords_sps_dist.append(get_distance(distance = 'SPS',
 	                                               root1=df1.chord_root_midi[idx_df1],
 	                                               root2=rdf2.chord_root_midi,
@@ -266,7 +274,7 @@ def get_progression(
 												   triad_reduction=triad_reduction,
 												   **sps_kws
 												   ))
-	
+
 			chords_vl_dist.append(get_distance(distance = 'voice leading',
 	                                               root1=df1.chord_root_midi[idx_df1],
 	                                               root2=rdf2.chord_root_midi,
@@ -279,7 +287,7 @@ def get_progression(
 												   triad_reduction=triad_reduction,
 	                                               **vl_kws
 												   ))
-	
+
 			chords_tbt_dist.append(get_distance(distance = 'tone by tone',
 	                                               root1=df1.chord_root_midi[idx_df1],
 	                                               root2=rdf2.chord_root_midi,
@@ -292,37 +300,39 @@ def get_progression(
 												   triad_reduction=triad_reduction,
 	                                               **tbt_kws
 												   ))
-			
+
 			chords_bin_dist.append(0 if df1_full_chord[idx_df1]==df2_full_chord[idx_df2] else 1)
-	
+			dcml_label.append(rdf2.label)
+
 			idx_df1 += 1
-		    
+
 		sps+=chords_sps_dist
 		vl +=chords_vl_dist
 		tbt+=chords_tbt_dist
 		binary+=chords_bin_dist
-		    
+
 		if len(matched_idx)>0:
-		        
+
 			time.append(df2_interval[idx_df2][0])
 			if len(matched_idx)>1:
 				for duration in matched_duration:
 					time.append(df2_interval[idx_df2][0]+duration)
 				del time[-1]
-		        
+
 			chords_df2 += [df2_full_chord[idx_df2]]*len(matched_idx)
 			chords_df1 += [df1_full_chord[idx_df1] for idx_df1 in matched_idx]
-			
+
 		durations+=matched_duration
-	            
+
 	progression = pd.DataFrame({'time':time,
 								 'matched_duration':durations,
-								 'annotation1_chord':chords_df1,
-								 'annotation2_chord':chords_df2,
+								 'annotation1_chord':chords_df2 if swap else chords_df1,
+								 'annotation2_chord':chords_df1 if swap else chords_df2,
+								 'dcml_label': dcml_label,
 								 'sps':sps,
 								 'vl':vl,
 								 'tbt':tbt,
-								 'binary':binary})	
+								 'binary':binary})
 	return progression
 
 
@@ -334,7 +344,7 @@ def plot_comparison(
 	title:str=None,
 	annotation1:str=None,
 	annotation2:str=None
-	): 
+	):
 	"""
 	Plot the progression of 2 annotaions
 
@@ -346,10 +356,10 @@ def plot_comparison(
 	figsize:
 		Specifies the size of the figure. The default is (22,10)
 	rge : float, optional
-		the range in terms of whole note of the progression : a 2-element array. 
+		the range in terms of whole note of the progression : a 2-element array.
 		The default is None.
 	verbose : bool, optional
-		if True the name of each chord of the two annotation will be marked under 
+		if True the name of each chord of the two annotation will be marked under
 		the dot of the event. The default is False.
 	title : str, optional
 		Title of the plot. The default is None.
@@ -378,10 +388,10 @@ def plot_comparison(
 	axs[1].set(xlabel='time', ylabel='distance to matched chord')
 
 	if verbose:
-        
+
 		limit1=progression.query('time>=@rge[0] and time<@rge[1]').index[0]
 		limit2=progression.query('time>=@rge[0] and time<@rge[1]').index[-1]
-        
+
 		plt.text(progression.loc[limit1].time, progression.loc[limit1].vl-0.2, progression.loc[limit1].annotation2_chord, horizontalalignment='center', verticalalignment='top', size='small', fontstretch ='normal', color='maroon')
 		for line in range(limit1+1,limit2):
 			 if progression.annotation2_chord[line] != progression.annotation2_chord[line-1]:
@@ -395,7 +405,7 @@ def plot_comparison(
 
 	if not title==None:
 		fig.suptitle(title);
-	
+
 	if not annotation1==None and not annotation2==None:
 		legend= annotation1 + ': upper chord\n' + annotation2 + ': lower chord'
 		fig.text(0.45, -0.02, legend)
